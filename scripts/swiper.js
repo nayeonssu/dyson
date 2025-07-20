5a6543c1ff570aee4ce67e9bc61e31e0666ba5b3
nayeonssu <sys7552@gmail.com>
2025-07-21 02:55:08 +0900

    다이슨 작업중

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,4 +1,6 @@
-const manSwiper = new Swiper('#ad_swiper', {
+/* ad 스와이퍼 */
+const adSwiper = new Swiper('#ad_swiper', {
+    scrollbar:{el:'#ad_swiper .swiper-scrollbar'},/* 스크롤바 */
     loop:true,
     autoplay:{delay:3000},
     direction:'horizontal',
@@ -6,5 +8,10 @@ const manSwiper = new Swiper('#ad_swiper', {
         prevEl:'.swiper-button-prev',
         nextEl:'.swiper-button-next',
     },
-    scrollbar:{el:'#ad_swiper ~ .swiper-scrollbar'},/* 스크롤바 */
+})
+/* 기술 스와이퍼 */
+const skillSwiper = new Swiper('#skill_swiper', {
+    loop:true,
+    /* autoplay:{delay:3000}, */
+    direction:'horizontal',
 })
